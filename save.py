6efb6049abfd3152bbb4d64dc6ba81299d9f287c
Nick Lalic <nick.lalic@gmail.com>
2017-09-25 21:02:10 -0700

    Updating with marker csv, screenshots

--- save.py
+++ save.py
@@ -17,11 +17,24 @@ def create_save_action(vna, path):
 		now = datetime.now()
 		print(now)
 		timestamp = now.strftime('%Y%m%d_%H%M%S')
+        mkpath(str(path / timestamp))
+        path      = path / timestamp
 		for i in vna.channels:
 			channel  = vna.channel(i)
-			filename = '{0}_ch{1}'.format(timestamp, i)
+			filename = 'ch{1}'.format(timestamp, i)
 			print('  Saving {0}'.format(filename))
 			filename = str(path / filename)
 			ports    = get_ports(vna, channel)
 			channel.save_measurement_locally(filename, ports)
+        for t in vna.traces:
+            trace = vna.trace(t)
+            for m in trace.markers:
+                marker = trace.marker(m)
+                marker.name
+                marker.x
+                marker.y
+            pass
+        for d in vna.diagrams:
+            # Save screenshot
+            pass
 	return action
