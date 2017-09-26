7419e21c13d6975ea2294ea4e389f03e83934355
Nick Lalic <nick.lalic@gmail.com>
2017-09-25 18:53:24 -0700

    Moved settings from run.py into settings.yaml

--- run.py
+++ run.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 
 from   homepath import home_path
+from   pathlib  import Path
 from   save     import create_save_action
 import schedule
 from   rohdeschwarz.instruments.vna import Vna
+from   ruamel   import yaml
 
-from pathlib import Path
-
-# Settings:
-interval_min    = 60
-duration_hr     = 24 * 7
-path            = Path('/your/path/here')
-vna_address     = '127.0.0.1'
+settings = {}
+with open('settings.yaml', 'r') as f:
+    settings = yaml.safe_load(f.read())
 
 # Connect to VNA
 vna = Vna()
-vna.open_tcp(vna_address)
+vna.open_tcp(settings['vna address'])
 vna.timeout_ms = 10*60*1000 # 10 mins
 
 # Create lamdba function.
 # Function will be called on interval
+path = Path(settings['path'])
 action = create_save_action(vna, path)
 
 # run
-schedule.run(interval_min, duration_hr, action)
+interval = settings['interval (mins)']
+duration = settings['duration (hrs)']
+schedule.run(interval, duration, action)
