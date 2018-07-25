a46ba96e0dde8261454dac7e571c2bca14043c14
Nick Lalic <nick.lalic@gmail.com>
2018-07-25 13:30:51 -0700

    small beautify

--- __main__.py
+++ __main__.py
@@ -18,7 +18,7 @@ vna.timeout_ms = 10*60*1000 # 10 mins
 
 # Create lamdba function.
 # Function will be called on interval
-path = expand_home_path(settings['path'])
+path   = expand_home_path(settings['path'])
 action = create_save_action(vna, path)
 
 # run
